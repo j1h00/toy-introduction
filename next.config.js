1e8cfce9dafe482ee7679aeeedbd1c513fc6660a
j1h00 <guruboy@snu.ac.kr>
2022-01-30 13:27:12 +0900

    2022-01-30

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const debug = process.env.NODE_ENV !== "production";
+const name = "portfolio";
+
 module.exports = {
   reactStrictMode: true,
-}
+  assetPrefix: !debug ? `/${name}/` : "",
+};
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 import "../styles/globals.css";
 import styles from "../styles/Home.module.css";
 
