a6e55bd0360b0f372f0104f1e7176934e5d978e1
j1h00 <guruboy@snu.ac.kr>
2022-05-22 23:18:01 +0900

    Deploy Next.js to gh-pages

--- _next/static/9YJbZL_RTSSiw3AosKFAN/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/67-fb35e5b3ff96dc5d.js","static/css/ebd6cd735e2931cb.css","static/chunks/pages/index-4393764be68fb62f.js"],"/_error":["static/chunks/pages/_error-8022dacb1937fc7a.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
