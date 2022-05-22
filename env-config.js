4bc85f4c53afe2c85fbde80d79848006d919eb84
j1h00 <guruboy@snu.ac.kr>
2022-05-22 23:23:36 +0900

    Update gh-pages url from portfolio to toy-introduction

--- env-config.js
+++ env-config.js
@@ -1,5 +1,5 @@
 const debug = process.env.NODE_ENV !== "production";
-const name = "portfolio";
+const name = "toy-introduction";
 
 module.exports = {
   "process.env.BACKEND_URL": !debug ? `/${name}` : "",
